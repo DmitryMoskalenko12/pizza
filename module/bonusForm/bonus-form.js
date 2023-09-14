31fc3a7b324c192285452f486749f72958376d5d
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-09-14 11:26:05 +0300

    start: develop bonusForm

---
+++ module/bonusForm/bonus-form.js
@@ -0,0 +1,13 @@
+import classes from './bonus-form.module.scss';
+
+const BonusForm = () => {
+  return (
+    <section className={classes.bonusForm}>
+      <div className="container">
+        
+      </div>
+    </section>
+  )
+}
+
+export default BonusForm;
