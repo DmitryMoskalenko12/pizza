5fa6ebcdeb470dacf4e2e4c2985448b379948c28
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-09-17 14:12:17 +0300

    add: validation to signIn and signOut

--- module/registration/registration.js
+++ module/registration/registration.js
@@ -10,11 +10,8 @@ import cn from 'classnames';
 
 const Registration = ({setSignIn}) => {
   const [signInOrSignUp, setSignInOrSignUp] = useState(false);
-  const [name, setName] = useState('');
-  const [phone, setPhone] = useState('');
   const [success, setSuccess] = useState();
   const router = useRouter();
-
 
   const showSignInOrSignUp = () => {
    setSignInOrSignUp(sign => !sign);
@@ -38,17 +35,12 @@ const Registration = ({setSignIn}) => {
 
   },[success])
  
-  const onRequest = async (e) => {
-   e.preventDefault();
+  const onRequest = async ({name, phone}) => {
    if (!signInOrSignUp) {
-    request(name, phone).then(res => {
+    request(name, String(phone)).then(res => {
       setSuccess(res.message)
-      setName('');
-      setPhone('');
     })
     .catch(e => {
-      setName('');
-      setPhone('');
       setSuccess(e.message)
     })
    } 
@@ -57,17 +49,13 @@ const Registration = ({setSignIn}) => {
     const result = await signIn('credentials', {
       redirect: false,
       name: name,
-      phone: phone,
+      phone: String(phone),
     })
 
     if (!result.error) {
       router.replace('/personalPage');
-      setName('');
-      setPhone('');
       setSuccess('Вхід дозволено')
     } else {
-      setName('');
-      setPhone('');
       setSuccess(result.error)
     }
    }
@@ -77,30 +65,49 @@ const Registration = ({setSignIn}) => {
     <div className={classes.formWrapper}>
       <button onClick={() => setSignIn(false)} className={classes.close}>&times;</button>
 
-      {!signInOrSignUp ? <form onSubmit={onRequest} className={classes.form}>
+      {!signInOrSignUp ? <Formik initialValues={{
+       name:'',
+       phone:''
+      }} validationSchema={Yup.object({
+        name: Yup.string().min(2, 'Мінімум 2 символи').required('Обов\'язкове поле'),
+        phone: Yup.string().matches(/^380\d{9}$/, 'Введіть корректний номер телефону').required('Обов\'язкове поле')
+      })} onSubmit={(values, {resetForm}) => {onRequest(values); resetForm()}}>
+        {({errors, touched}) =>
+        <Form className={classes.form}>
         <HeaderH h={'h2'} clazz={classes.h2}>Реєстрація</HeaderH>
         <label className={classes.nameLabel} htmlFor="name">Ім'я</label>
-        <input onChange={(e) => setName(e.target.value)} className={classes.name} id='name' value={name} type="text" required placeholder="Ім'я"/>
-
+        <Field className={cn(classes.name, {[classes.errorInput]: errors.name && touched.name})} id='name' type="text" name='name' required placeholder="Ім'я"/>
+        <ErrorMessage className={classes.error} name='name' component={'div'}/>
         <label className={classes.phoneLabel} htmlFor="phone">Телефон</label>
-        <input className={classes.phone} onChange={(e) => setPhone(e.target.value)} id='phone' value={phone} type="number" required placeholder="Телефон"/>
+        <Field className={cn(classes.phone, {[classes.errorInput]: errors.phone && touched.phone})} id='phone' type="number" name='phone' required placeholder="380999999999"/>
+        <ErrorMessage className={classes.error} name='phone' component={'div'}/>
         <div className={cn({
           [classes.success]: success === 'Обліковий запис успішно створено',
           [classes.fail]: success !== 'Обліковий запис успішно створено'})}>{success}</div>
-        <button className={classes.button}>Зареєструватися</button>
-      </form>
+        <button type='submit' className={classes.button}>Зареєструватися</button>
+      </Form>}
+      </Formik>
        :
-      <form onSubmit={onRequest} className={classes.form}>
-        <HeaderH h={'h2'} clazz={classes.h2}>Увійти на сайт</HeaderH>
-        <label className={classes.nameLabel} htmlFor="name">Ім'я</label>
-        <input onChange={(e) => setName(e.target.value)} className={classes.name} id='name' value={name} type="text" required placeholder="Ім'я"/>
-        <label className={classes.phoneLabel} htmlFor="phone">Телефон</label>
-        <input onChange={(e) => setPhone(e.target.value)} className={classes.phone} id='phone' type="number" value={phone} required placeholder="Телефон"/>
-        <div className={cn({
-          [classes.success]: success === 'Вхід дозволено',
-          [classes.fail]: success !== 'Вхід дозволено'})}>{success}</div>
-        <button className={classes.button}>Увійти</button>
-      </form>}
+       <Formik initialValues={{name: '', phone: ''}} validationSchema={Yup.object({
+        name: Yup.string().min(2, 'Мінімум 2 символи').required('Обов\'язкове поле'),
+        phone: Yup.string().matches(/^380\d{9}$/, 'Введіть корректний номер телефону').required('Обов\'язкове поле')
+      })} onSubmit={(values, {resetForm}) => {onRequest(values); resetForm()}}>
+        {({errors, touched}) =>
+        <Form className={classes.form}>
+          <HeaderH h={'h2'} clazz={classes.h2}>Увійти на сайт</HeaderH>
+          <label className={classes.nameLabel} htmlFor="name">Ім'я</label>
+          <Field className={cn(classes.name, {[classes.errorInput]: errors.name && touched.name})} id='name' name='name' type="text" required placeholder="Ім'я"/>
+          <ErrorMessage className={classes.error} name='name' component={'div'}/>
+          <label className={classes.phoneLabel} htmlFor="phone">Телефон</label>
+          <Field className={cn(classes.phone, {[classes.errorInput]: errors.phone && touched.phone})}  id='phone' name='phone' type="number" required placeholder="Телефон"/>
+          <ErrorMessage className={classes.error} name='phone' component={'div'}/>
+          <div className={cn({
+            [classes.success]: success === 'Вхід дозволено',
+            [classes.fail]: success !== 'Вхід дозволено'})}>{success}</div>
+          <button className={classes.button}>Увійти</button>
+        </Form>}
+       </Formik>
+     }
 
       <button onClick={showSignInOrSignUp} className={classes.logInOrSignUp}>{!signInOrSignUp ? 'Увійти з існуючим обліковим записом' : 'Створити новий аккаунт'}</button>
     </div>
