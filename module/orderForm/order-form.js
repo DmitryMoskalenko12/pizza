b70ff0f5e1930fb95e410ff048ff8827a3873ca1
Dima Moskalenko <dmtrmsklnk@gmail.com>
2023-09-20 00:06:47 +0300

    add: validation and musk to input

--- module/orderForm/order-form.js
+++ module/orderForm/order-form.js
@@ -1,6 +1,6 @@
 import HeaderH from '@/ui/headerH/headerH';
 import classes from './order-form.module.scss';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
+import { Formik, Field, Form, ErrorMessage, useField } from 'formik';
 import * as Yup from 'yup';
 import cn from 'classnames';
 import BasketOrderInfo from '../basketOrderInfo/basketOrderInfo';
@@ -12,6 +12,7 @@ import useHttp from '@/hooks/http.hook';
 import { clearBasketArr, getFinalPrice, getSuccessMessage } from '@/components/basket/basketSlice';
 import Success from '../success/success';
 import { useRouter } from 'next/router';
+import ReactInputMask from 'react-input-mask';
 
 const OrderForm = () => {
   const basketArr = useSelector(state => state.basket.basketArr);
@@ -19,6 +20,7 @@ const OrderForm = () => {
   const dispatch = useDispatch();
   const [message, setMessage] = useState('');
   const [signIn, setSignIn] = useState(false);
+  const [choice, setChoice] = useState('');
   const router = useRouter();
   const {request} = useHttp();
 
@@ -36,7 +38,7 @@ const OrderForm = () => {
       dispatch(getSuccessMessage(''));
       router.push('/')
      },4000);
-     
+
      () => clearTimeout(timer);
     }
   },[message])
@@ -84,15 +86,27 @@ const OrderForm = () => {
       date: '',
       cvc: ''
     }}
-    validationSchema={Yup.object({
+    validationSchema={Yup.object().shape({
       name: Yup.string().min(2, 'Мінімум 2 символи').required('Обов\'язкове поле'),
       phone: Yup.string().matches(/^380\d{9}$/, 'Введіть корректний номер телефону').required('Обов\'язкове поле'),
       address: Yup.string().required('Обов\'язкове поле'),
       timeDelivery: Yup.string().required('Обов\'язкове поле'),
       card: Yup.string().oneOf(['1', '2']).required('Виберіть хоча б один варіант оплати'),
-      numberCard: Yup.string().required('Обов\'язкове поле'),
-      date: Yup.string().required('Дата картки обов\'язкова'),
-      cvc: Yup.string().required('CVC обов\'язкове')
+      numberCard: Yup.string().when('card', {
+        is: '1',
+        then: () => Yup.string().required('Обов\'язкове поле'),
+        otherwise: () => Yup.string()
+      }),
+      date: Yup.string().when('card', {
+        is: '1',
+        then: () => Yup.string().matches(/^(0[1-9]|1[0-9]|2[0-9])\/(0[1-9]|1[0-9]|2[0-9]|30)$/, 'Введіть правильну дату').required('Дата картки обов\'язкова'),
+        otherwise: () => Yup.string()
+      }),
+      cvc: Yup.string().when('card', {
+        is: '1',
+        then: () => Yup.string().required('CVC обов`язкове'),
+        otherwise: () => Yup.string()
+      }),
     })} onSubmit={(values, {resetForm}) => {sendDataToDB(values); resetForm()}}>
       {({errors, touched}) =>
       <Form className={classes.orderForm}>
@@ -139,12 +153,24 @@ const OrderForm = () => {
               </label>
             </div>
             <ErrorMessage name="card" className={classes.error2} component={'div'}/>
-            <Field className={classes.numberCard} type='number' name="numberCard" placeholder='Номер картки'/>
+            <Field name='numberCard'>
+              {({ field }) => (
+                <ReactInputMask name={field.name} mask='9999-9999-9999-9999' className={classes.numberCard} type='text' placeholder='XXXX-XXXX-XXXX-XXXX' onChange={field.onChange} onBlur={field.onBlur}/>
+              )}
+            </Field>
             <ErrorMessage name="numberCard" className={classes.error2} component={'div'}/>
             <div className={classes.cvcBlock}>
-              <Field type="number" name="date" className={classes.date} placeholder="20/23"/>
+              <Field name='date'>
+              {({ field }) => (
+                <ReactInputMask mask='99/99' name={field.name} type="text" maskChar={null} className={classes.date} placeholder="20/23" onChange={field.onChange} onBlur={field.onBlur}/>
+              )}
+              </Field>
               <div className={classes.cvcWrap}>
-              <Field type="number" name="cvc" className={classes.cvc} placeholder="CVC"/>
+              <Field name='cvc'>
+              {({ field }) => (
+                <ReactInputMask mask='999' type="text" maskChar={null} name={field.name} className={classes.cvc} onChange={field.onChange} onBlur={field.onBlur} placeholder="CVC"/>
+               )}
+               </Field>
               </div>
             </div>
             <ErrorMessage name="date" className={classes.error2} component={'div'}/>
